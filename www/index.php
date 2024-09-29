c29035264439b33c90712b5e09cde913415741d4
Oleksii R <sorrowerkein+bitbucket@gmail.com>
2024-09-30 01:41:18 +0200

    add cors support

--- www/index.php
+++ www/index.php
@@ -1,6 +1,16 @@
 <?php
 
-declare(strict_types=1);
+
+/* Handle CORS */
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Headers: X-Requested-With,Authorization,Content-Type');
+header('Access-Control-Max-Age: 86400');
+
+if (strtolower($_SERVER['REQUEST_METHOD']) == 'options') {
+    header("HTTP/1.1 204");
+    exit();
+}
 
 require __DIR__ . '/../vendor/autoload.php';
 
